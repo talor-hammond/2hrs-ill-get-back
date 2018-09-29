0ef339c81ac1db82650a1ef92291b0896712bede
talor-hammond <talorjhammond@gmail.com>
2018-09-29 21:08:06 +1200

    method which converts inputs into an array of movie titles

--- Program.cs
+++ Program.cs
@@ -12,13 +12,17 @@ namespace hrsillnevergetback
             GetAppInfo("2hrs I Won't Need Back", "Talor Hammond");
 
             // Gather / return all the input movie titles into an array
-            Console.WriteLine("What movie titles did you want to compare? [separate by comma, ',']");
-            string[] movieTitles = Console.ReadLine().Split(','); // should return an array of strings into the movieTitles variable
+            //Console.WriteLine("What movie titles did you want to compare? [separate by comma, ',']");
+            //string[] movieTitles = Console.ReadLine().Split(','); // should return an array of strings into the movieTitles variable
 
-            foreach(string title in movieTitles) {
-                Console.WriteLine(title);
-            }
+            //foreach (string title in movieTitles)
+            //{
+            //    Console.WriteLine(title);
+            //}
 
+            // Gather / return all the input movie titles into an array:
+            GetMovieTitles();
+           
             // Request to api w movieTitle:
 
         }
@@ -28,6 +32,30 @@ namespace hrsillnevergetback
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("{0}, by {1}", appName, appAuthor);
             Console.ResetColor(); // method exposed by the Console class to reset fg & bg colours
+        }
+
+        static void GetMovieTitles() {
+            Console.WriteLine("What movie titles did you want to compare? [enter 'q' when finished]");
+            bool stillEnteringTitles = true;
+            List<string> movieTitles = new List<string>();
+
+            while (stillEnteringTitles)
+            {
+                string input = Console.ReadLine();
+
+                if (input != "q")
+                { // If the user hasn't exit'd the program w 'q', add the input to our array
+                    movieTitles.Add(input);
+                }
+                else
+                {
+                    stillEnteringTitles = false;
+                }
+            }
+
+            foreach (string title in movieTitles.ToArray()) {
+                Console.WriteLine(title);
+            }
         }
     }
 }
