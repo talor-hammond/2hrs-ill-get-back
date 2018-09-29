b295d65695b8f5ff99d4fe95d3e4ebec8e2c5109
talor-hammond <talorjhammond@gmail.com>
2018-09-29 19:25:30 +1200

    initialise project, 'Hello world'

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System; // makes the 'Console' class available to the program
+
+namespace hrsillnevergetback
+{
+    // Primary class:
+    class Program
+    {
+        // 'Entry-point' method
+        public static void Main(string[] args) //  'static'; no 'instances'
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
