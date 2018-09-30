babb6fe42b59fdca086429e6dbd528e80b896a7a
talor-hammond <talorjhammond@gmail.com>
2018-09-30 18:07:22 +1300

    grabbing particular title innerText

--- Program.cs
+++ Program.cs
@@ -29,8 +29,8 @@ namespace Hoursillgetback
             HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb(); // our disposable browser
             HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.imdb.com/search/title?genres=thriller&sort=user_rating,desc&title_type=tv_series,mini_series&num_votes=5000,&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=f85d9bf4-1542-48d1-a7f9-48ac82dd85e7&pf_rd_r=7NT5BKR5TETJR3812F9T&pf_rd_s=right-6&pf_rd_t=15506&pf_rd_i=toptv&ref_=chttvtp_gnr_8");
 
-            var titleNodes = doc.DocumentNode.SelectSingleNode("//h3").InnerText;
-            Console.WriteLine(titleNodes);
+            var titleNode = doc.DocumentNode.SelectSingleNode("//h3[@class='lister-item-header']").SelectSingleNode(".//a").InnerText;
+            Console.WriteLine(titleNode);
 
             while (enteringByTitles) // TODO: need to wrap this functionality into its own program
             {
